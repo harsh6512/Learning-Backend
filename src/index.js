cb735cbf75af3a45c2566cce88b4096d3b0807fb
harsh6512 <harshkumar1987ynr@gmail.com>
2024-12-18 17:01:14 +0530

    connecting database with backend

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv'
+
+
+dotenv.config({
+    path:'./.env'
+})
+
+connectDB()
+.then(()=>{
+     app.listen(process.env.PORT || 8000,()=>{
+        console.log(`The server is running at the PORT ${process.env.PORT}`)
+     })
+})
+.catch((error)=>{
+    console.log("Mongo db connection failed",error)
+})
+
+
+
+
+
+/*
+import express from "express";
+const app=express();
+
+(async()=>{
+   try{
+    mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error",(error)=>{
+        console.log("ERR: ",Error);
+        throw error;
+    })
+
+    app.listen(process.env.PORT,()=>{
+        console.log(`the app is listening on the port ${process.env.PORT}`)
+    })
+   }catch(error){
+    console.error("ERROR: ", error);
+    throw error;
+   }
+})()
+   */
