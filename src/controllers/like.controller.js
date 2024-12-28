7f3d83f719def306e54cb97adffe47213638a089
harsh6512 <harshkumar1987ynr@gmail.com>
2024-12-28 23:30:56 +0530

    added the toggling tweet and comment functionality

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -27,7 +27,7 @@ const toggleVideoLike=asyncHandler(async(req,res)=>{
     if(!alreadyliked){
         const newLike=await Like.create(likedCriteria)
         if(!newLike){
-            throw new ApiError(400,"unable to like the video")
+            throw new ApiError(500,"unable to like the video")
         }
         return res
         .status(200)
@@ -35,9 +35,86 @@ const toggleVideoLike=asyncHandler(async(req,res)=>{
     }
     const dislike=await Like.deleteOne(likedCriteria)
     if(!dislike){
-        throw new ApiError(400,"unable to dislike the video at the moment")
+        throw new ApiError(500,"unable to dislike the video at the moment")
     }
     return res.
     status(200).
     json(new ApiResponse(200, {}, "video disliked"))
 })
+
+const toggleCommentLike=asyncHandler(async(req,res)=>{
+    const {commentId}=req.params
+    if(!commentId){
+        throw new ApiError(400,"comment id is required")
+    }
+
+    const comment=await Comment.findById(commentId)
+    if(!comment){
+        throw new ApiError(400,"Comment not found")
+    }
+
+    const likedCriteria={
+        comment:commentId,
+        likedBy:req?.user._id
+    }
+    const alreadyliked=await Like.findOne(likedCriteria)
+    if(!alreadyliked){
+        const newLike=await Like.create(likedCriteria)
+        if(!newLike){
+            throw new ApiError(500,"unable to like the comment at the moment")
+        }
+        return res
+        .status(200)
+        .json(200,newLike,"Comment liked successfully")
+    }
+    const dislike=await Like.deleteOne(likedCriteria);
+    if(!dislike){
+        throw new ApiError(500,"unable to dislike the comment at the moment")
+    }
+    return res
+    .status(200)
+    .json(200,{},"Comment disliked succesfully")
+})
+
+const toggleTweetLike=asyncHandler(async(req,res)=>{
+    const {tweetId}=req.params
+    if(!tweetId){
+        throw new ApiError(400,"Tweet id is required")
+    }
+
+    const tweet=Tweet.findById(tweetId)
+    if(!tweet){
+        throw new ApiError(400,"Tweet not found")
+    }
+    
+    const likedCriteria={
+        tweet:tweetId,
+        likedBy:req?.user._id
+    }
+
+    const alreadyliked=await Like.findOne(likedCriteria)
+    if(!alreadyliked){
+        const newLike=await Like.create(likedCriteria)
+        if(!newLike){
+            throw new ApiError(500,"unable to like the tweet at the moment")
+        }
+        return res
+        .status(200)
+        .json(new ApiResponse(200, newLike, "Tweet liked successfully"));
+    }
+
+    const dislike=await Like.deleteOne(likedCriteria)
+    if(!dislike){
+        throw new ApiError(500,"unable to dislike the comment at the moment")
+    }
+    return res
+    .status(200)
+    .json(200,{},"Tweet disliked succesfully")
+
+})
+
+export {
+    toggleVideoLike,
+    toggleCommentLike,
+    toggleTweetLike,
+}
