7e8896356de9e57535afbe9083da3907c5412210
harsh6512 <harshkumar1987ynr@gmail.com>
2024-12-31 17:48:46 +0530

    minor changes

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,10 +1,10 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import {Like} from "../models/like.model.js";
 import {Comment} from "../models/comment.model.js"
-import {Tweet} from "../models/tweet.model.js"
-import {Video} from "../models/video.model.js"
+import {Tweet} from "../models/tweet.model.js";
+import {Video} from "../models/video.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js"
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
 
 const toggleVideoLike=asyncHandler(async(req,res)=>{
